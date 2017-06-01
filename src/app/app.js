858ecede57c24204ebf01ab83c28509def58d38b
Tyler Saballus <tysabs@gmail.com>
2017-06-01 18:03:38 -0500

    Change app.js hello world logic - Bugfix react preset

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,11 @@
 console.log("Hello Joe!");
 
-const header = document.getElementById("test");
+const header = document.getElementById("app-test");
 
+const source = ['1', '1', 'foo', '4', '5', 'baz', '3', '22', 'bar', '2', 'App Works!'];
 
-
-var source = ['1', '1', 'foo', '4', '5', 'baz', '3', '22', 'bar', '2'];
-
-var result = source
-	.map(x => parseInt(x))
-	.filter(x => !isNaN(x))
-	.reduce((x, y) => x + y);
+const result = source
+	.filter(x => isNaN(x))
+	.reduce((x, y) => x + " " + y + " ");
 
 header.innerHTML = result;
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         exclude: [/node_modules/],
         use: [{
           loader: 'babel-loader',
-          options: { presets: ['es2015', 'react'] }
+          options: { presets: ['es2015'] }
         }]
       }
     ]
