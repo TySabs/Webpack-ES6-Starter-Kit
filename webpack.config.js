8c24077108aacde6e38f97f1aca0b5da8a701603
Tyler Saballus <tysabs@gmail.com>
2017-06-01 17:51:16 -0500

    💣 init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: path.resolve(__dirname, './src/app/app.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './',
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015', 'react'] }
+        }]
+      }
+    ]
+  }
+};
